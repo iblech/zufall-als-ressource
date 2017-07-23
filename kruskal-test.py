0368c2b1529862ebee72e58dec1f4449827638c5
Ingo Blechschmidt <iblech@web.de>
2017-07-23 12:29:23 +0200

    Beispielparameter für heute Abend

--- kruskal-test.py
+++ kruskal-test.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from __future__ import division
@@ -5,20 +6,21 @@ from __future__ import print_function
 
 import numpy as np
 
-N = 60  #Anzahl der Karten
-K = 20 # Anzahl der Kinder
-M = 2000 # Anzahl der Durchgänge
+K = 16     # Anzahl der Kinder
+N = K * 7  # Anzahl der Karten
+M = 2000   # Anzahl der Durchgänge
+D = 7      # Anzahl der Seiten des Würfels
+S = 9      # Anzahl erlaubter Startpositionen
 
 sum_of_frequency = 0
 
 for i in range(M): #verschiedene Verteilungen der Klassen
-
-    cards = np.random.randint(10, size=N) + 1
+    cards = np.random.randint(S, size=N) + 1
 
     end_positions = np.zeros(N)
 
     for i in range(K): #die ganze Klasse. wie viele Kindes haben das gleiche ergebnis
-        pos = np.random.randint(10)
+        pos = np.random.randint(D)
 
         while True: #ein Kind
             step = cards[pos]
@@ -33,5 +35,6 @@ for i in range(M): #verschiedene Verteilungen der Klassen
     single_frequency = np.amax(end_positions)/K
 
     sum_of_frequency += single_frequency
+    print(single_frequency)
 
-print("Die durchschnittliche Frequenz: %f" %(sum_of_frequency/M))
+#print("Die durchschnittliche Frequenz: %f" %(sum_of_frequency/M))
